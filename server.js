58cf038ec7ed13f5ad8a58fed7368f8fb02f13bd
Vlad Shusterman <vlad.shusterman30@gmail.com>
2019-05-20 01:19:46 +0300

    'a'

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-app.listen(process.env.PORT || config.port,
-  () => console.log(`Server start on port ${config.port} ...`))
+
 const Post = require('./src/models/post-model')
 const User = require('./src/models/user-model')
 
